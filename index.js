5ef2cfcc25cf8c6f7dbcc8208c1f7912e43c3a2d
leandroffdev <leandrofrancagti@gmail.com>
2025-04-21 13:55:41 +0100

    tipos de dados primitivos

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ console.log("ola mundo")
 function clickMe() {
     console.log("clicou")
 }
+
+let height = 180;
+let weight = 110;
+const conversionFactor = 0.01;
+height *= conversionFactor;
+
+let imc = weight / (height * height);
+const result = `O resultado do IMC é de ${imc}"`
+const isHealthy = true;
+const isNull = null     //inicializado sem valor
+
+console.log(typeof teste);      // teste não foi definido
