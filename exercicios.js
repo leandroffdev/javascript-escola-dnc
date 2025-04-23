8b70951c445d00dc4c5109c6f0dafd1b9c62774b
leandroffdev <leandrofrancagti@gmail.com>
2025-04-23 14:38:23 +0100

    Fim dos exercicios

--- exercicios.js
+++ exercicios.js
@@ -20,6 +20,7 @@ do{
     k++;
     console.log(k)
 }while(k < 3)
+    
 // 4.Escreva um laço for que imprime os números pares de 2 a 20 no console.
 console.log("Exercicio 4")
 for(let l = 0; l <=20;l++) {
@@ -28,10 +29,31 @@ for(let l = 0; l <=20;l++) {
     }
 }
 // 5.Crie um laço while que imprime os números de 10 a 1 no console (em ordem decrescente).
+console.log("Exercicio 5");
+let m = 10;
+while (m >= 1) {
+    console.log(m); 
+    m--;
+}
 
 // 6.Escreva um laço for que imprime a tabuada do 5 (5, 10, 15, ..., 50).
+console.log("Exercício 6");
+for(let n = 1; n <= 10; n++) {
+    console.log(n * 5);
+}
 
 // 7.Crie um laço for que calcula e imprime a soma dos números de 1 a 100.
+console.log("Exercício 7");
+let soma = 0;
+for (let o = 1; o <= 100; o++) {
+    soma += o;
+}
+console.log(soma);
 
 // 8.Escreva um laço while que imprime apenas os números ímpares de 1 a 20.
+let p = 1;
+while (p <= 20) {
+    console.log(p); 
+    p += 2;
+}
 
